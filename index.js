8af736ed23dc0ecf1e8731b84e510adb9118e2ee
Wanidx <s6506021610045@kmutnb.ac.th>
2024-01-30 10:10:05 +0700

    main

--- index.js
+++ index.js
@@ -82,5 +82,5 @@ app.delete('/books/:id', (req, res) => {
     });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`))
